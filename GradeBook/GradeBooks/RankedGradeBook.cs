f51e22b308589d11529cffa31a7aee331499b89b
RafalMederakWSEI <rafal.mederak@microsoft.wsei.edu.pl>
2022-03-30 13:06:24 +0200

    Override RankedGradeBook's CalculateStatistics method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,5 +35,12 @@ namespace GradeBook.GradeBooks
             else if (studentsHigherThanInputAverage <= numberOfStudents * 0.8) return 'D';
             else return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5) Console.Write("Ranked grading requires at least 5 students.");
+            else if(Students.Count >= 5) base.CalculateStatistics();
+        }
+
     }
 }

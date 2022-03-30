e901b4a913d90befad06ce75b28b4e0235b28219
RafalMederakWSEI <rafal.mederak@microsoft.wsei.edu.pl>
2022-03-30 19:34:25 +0200

    Update BaseGradeBook.GetGPA.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,20 +110,36 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int points = 0;
+          
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    points = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    points = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    points = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    points = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    points = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted)
+            {
+                if(studentType == StudentType.Honors | studentType == StudentType.DualEnrolled)
+                {
+                    points++;
+                }             
+            }
+
+            return points;
         }
 
         public virtual void CalculateStatistics()
